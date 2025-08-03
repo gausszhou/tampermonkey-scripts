38f01f2286a6a21188a3bc960d9484e991787e41
gausszhou <gausszhou@qq.com>
2025-08-04 01:28:11 +0800

    refactor(do-not-track): 简化主函数并移除严格模式
    移除立即执行函数和严格模式声明，使代码更简洁

--- packages/do-not-track/src/main.js
+++ packages/do-not-track/src/main.js
@@ -1,5 +1,6 @@
-(function() {
-    'use strict';
-    window.doNotTrack = 1;
-    Object.defineProperty(navigator, 'doNotTrack', { get() {return 1 }});
-})();
+window.doNotTrack = 1;
+Object.defineProperty(navigator, "doNotTrack", {
+  get() {
+    return 1;
+  }
+});
