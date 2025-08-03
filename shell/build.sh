b477afce6146fd0e6b65da4c970887595bbede91
gausszhou <gausszhou@qq.com>
2025-08-04 01:28:27 +0800

    build: 添加 do-not-track 项目的构建命令

--- shell/build.sh
+++ shell/build.sh
@@ -5,5 +5,6 @@ pnpm --filter csdn-greener build
 pnpm --filter douyu-greener build
 pnpm --filter novel-reader build
 pnpm --filter util-spacingjs build
+pnpm --filter do-not-track build
 
 mv ./packages/**/dist/**.user.js  ./dist/
