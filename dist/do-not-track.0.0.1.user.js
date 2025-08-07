9554a70bf9624b17a7742b52cd16b039cd1987a5
gausszhou <gausszhou@qq.com>
2025-08-08 00:15:20 +0800

    fix(do-not-track): 更新脚本版本号并优化匹配规则
    将版本号从日期格式改为语义化版本0.0.1
    修改@match规则为更精确的*://**/*
    移除不必要的严格模式声明并优化代码格式

--- dist/do-not-track.0.0.1.user.js
+++ dist/do-not-track.0.0.1.user.js
@@ -1,20 +1,20 @@
 // ==UserScript==
 // @name         Do Not Track
 // @namespace    http://tampermonkey.net/
-// @version      2025-06-17
+// @version      0.0.1
 // @description  try to take over the world!
 // @author       You
-// @match        *
-// @match        *://*/*
+// @match        *://**/*
 // @icon         https://www.gausszhou.top/favicon.ico
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
 /******/ (() => { // webpackBootstrap
-(function() {
-    'use strict';
-    window.doNotTrack = 1;
-    Object.defineProperty(navigator, 'doNotTrack', { get() {return 1 }});
-})();
+window.doNotTrack = 1;
+Object.defineProperty(navigator, "doNotTrack", {
+  get() {
+    return 1;
+  }
+});
 /******/ })()
 ;
